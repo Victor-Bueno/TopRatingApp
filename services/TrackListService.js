0d78d8c878f3c233de40038f7191a3ec72011ce0
Victor-Bueno <victor.bueno0720@gmail.com>
2020-03-24 11:17:11 -0300

    Track List Service implemented

---
+++ services/TrackListService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const MMKey = "df305db8d07b8dfe85a2419fda233462";
+
+export default class TrackListService {
+    static async getTrackListData(){
+        let trackListData = [];
+        await axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=br&f_has_lyrics=1&apikey=${MMKey}`)
+        .then(res => trackListData = res.data)
+        .catch(err => console.log(err));
+        return trackListData;
+    }
+}
