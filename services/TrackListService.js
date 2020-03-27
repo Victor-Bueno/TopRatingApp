e5aa95b055f96cad944efd16aae7aea4f4cf9a66
Victor-Bueno <victor.bueno0720@gmail.com>
2020-03-27 14:19:01 -0300

    Error treatment fix

--- services/TrackListService.js
+++ services/TrackListService.js
@@ -5,7 +5,7 @@ const MMKey = "df305db8d07b8dfe85a2419fda233462";
 export default class TrackListService {
     static async getTrackListData(countrySelected){
         let res = await axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=${countrySelected}&f_has_lyrics=1&apikey=${MMKey}`);
-        if (res.data.message.header.status_code != 200) {
+        if (res.data.message.header.status_code < 200 || res.data.message.header.status_code >= 300) {
             throw Error('Error ' + res.data.message.header.status_code);
         }
         return res.data;
